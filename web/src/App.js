a0458302a252b51a53cf784e35fbbb4efb60406b
Darbaz Ali <darbaz.me@gmail.com>
2021-06-10 17:54:24 +0300

    setup react app

---
+++ web/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello there!</h1>
+      <p>Welcome to Notedly Application</p>
+    </div>
+  )
+}
+
+export default App
