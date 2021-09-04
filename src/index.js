a2f214548c5f0ecf802d45c1b78f3813da9803a1
Darbaz Ali <darbaz.me@gmail.com>
2021-09-04 10:12:36 +0300

    setup apollo server

--- src/index.js
+++ src/index.js
@@ -2,15 +2,34 @@
 console.clear()
 
 import express from "express"
+import { ApolloServer, gql } from "apollo-server-express"
 
 const app = express()
 
 const port = process.env.PORT || 5000
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hello World!</h1>")
-})
+// construct a scheam using GraphQl Schema Language
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`
+
+// Provide resolver functions for our schema fields
+const resolvers = {
+    Query: {
+        hello: () => "Hello World",
+    },
+}
+
+// apollo server setup
+const server = new ApolloServer({ typeDefs, resolvers })
+
+// Apply the Apollo GraphQl middleware and set the path to /api
+server.applyMiddleware({ app, path: "/api" })
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+    console.log(
+        `GraphQl Server running at http://localhost:${port}${server.graphqlPath}`
+    )
 })
